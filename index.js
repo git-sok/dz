bfb2efb3ba49266d7a2bc8c488b75bf0d5c74a06
s-0-k <blinetpamyti@gmail.com>
2026-07-03 14:57:24 +0300

    dz почти все

--- index.js
+++ index.js
@@ -1,129 +1,59 @@
-////////////////////////______________DZDZDZDZ________//////////////////////////////
 
-        
-    //ОДИНАКОВАЯ СТРОКА
+/////////////////////___DZ_____DZ____DZ______02.07.2026_____DZ____DZ______DZ___//////////////////////
 
 
+let par = document.getElementById('pes')
 
-// const isPalindrome = () => {
+console.log(par);
+let btn = document.querySelector('#btn-fon')
 
-//     let str = prompt();
-//     let masStr = str.split('');
-//     let revMass = masStr.reverse();
-//     let revStr = revMass.join('');
+btn.addEventListener(
+    'click',
+    () => par.textContent = 'Измененный текст после кнопки'
+)
+let imgScale = document.getElementById('scale')
 
-//     if (revStr === str){
-//         return true;
-//     }else{
-//         return false;
-//     }
-// }
-// console.log(isPalindrome());
+imgScale.addEventListener(
+    'mouseenter',
+    ()=>{
+        imgScale.style.transition = 'transform 0.5s ease';
+        imgScale.style.transform = 'scale(1.4)';
+    }
+)
+imgScale.addEventListener(
+    'mouseleave',
+    ()=>{
+       imgScale.style.transition = 'transform 0.5s ease';
+        imgScale.style.transform = 'scale(1)'; 
+    }
+)
 
+let spisok = document.getElementById("olSpis")
+let addElem = document.getElementById("addEL")
 
+addElem.addEventListener('click', () => {
+    let newEl = document.createElement('li')
+    newEl.textContent = 'Новый элемент';
+    spisok.appendChild(newEl);
+})
 
+let dellElem = document.getElementById('delEl')
 
-// const isPalindrome = () => {
-    
-//     let str = prompt();
-//     let masStr = str.split('');
-//     let revMass = masStr.reverse();
-//     let revStr = revMass.join('');
+dellElem.addEventListener('click', ()=> {
 
-//     return revStr === str;
-// }
-// console.log(isPalindrome());
-
-
-
-
-// const isPalindrome = () => {
-//     let str = prompt();
-//     let revStr = str.split('').reverse().join('');
-//     return str === revStr;
-// }
-// console.log(isPalindrome());
-
-
-
-
-// const isPalindrome = () => {
-
-//     let str = prompt();
-//     return str === str.split('').reverse().join('');
-// }
-// console.log(isPalindrome());
+        let lastEL = spisok.lastElementChild;
+        if(lastEL){
+        lastEL.remove();
+}
+});
 
 
+let input = document.getElementById('inputCopy')
+let pCopy = document.getElementById('copy')
 
-///////////////////////////////////////////////////////////////////////////
-
-        //КАКОЙ ЧАС И ВРЕМЯ СУТОК
-
-// let tekushee = new Date();
-// console.log(tekushee);
-
-// let hours = tekushee.getHours();
-// console.log(hours);
-// if (hours >=6 && hours <= 11){
-//     console.log('Утро');
-// }else if (hours >11 && hours <= 18){
-//     console.log('День');
-// }else if (hours >18 && hours <= 23){
-//     console.log('Вечер');
-// }else{
-//     console.log('Ночь')
-// }
-
-
-//////////////////////////////////////////////////////////////////////////
-
-    //РАБОТА С ТЕКСТОМ
-
-// let str = "  JavaScript - это круто!  ";
-// let redact = str.trim().toUpperCase();
-// console.log(redact);
-
-
-//////////////////////////////////////////////////////////////////////////
-
-    //УЗНАЕМ СКОЛЬКО ЛЕТ
-
-// let dr = new Date("2005-05-21");
-// let now = new Date();
-
-// let diff = now - dr;
-// let days = (diff / (1000*60*60*24)) / 365;
-// let year = Math.floor(days);
-// console.log(year);
-
-
-//////////////////////////////////////////////////////////////////////////
-
-    //ПРЕВРАЩАЕМ В ИНИЦИАЛЫ ИФО
-
-// const shortName = () => {
-//     let full = prompt('Введите полное Имя Фамилия Отчество');
-//     console.log(full);
-    
-//     let short = full.split(' ');
-//     console.log(short);
-    
-//     let lastName = short[0].charAt(0).toUpperCase() + short[0].slice(1).toLowerCase();
-//     let name = short[1].slice(0, 1).toUpperCase();
-//     let surName = short[2].slice(0, 1).toUpperCase();
-//     console.log(`${lastName} ${name}. ${surName}.`);
-// }
-// shortName();
-
-
-/////////////////////////////////////////////////////////////////////////////////
-
-    //СЧИТАЕМ КОЛИЧЕСТВО СЛОВ
-
-// function countWords (str) {
-//     if (str == '') return 'НУ ты ЖЕ ниЧЕго НЕ ВВел...';
-//     let word = str.trim().split(" ");
-//     return word.length;
-// }
-// console.log(countWords('Один Два Три'));
+input.addEventListener(
+    'input',
+    () => {
+        pCopy.textContent = input.value
+    }
+)
